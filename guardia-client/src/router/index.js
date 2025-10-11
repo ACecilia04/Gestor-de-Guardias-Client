64fe0f663bc449b5ce4cf1b5bdf5c6ac1acc7188
Amanda Cecilia Aguilera García <amandaaguileragarcia@gmail.com>
2025-10-11 12:44:04 -0400

    first

---
+++ guardia-client/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/home/Home.vue'
+import Login from '../views/login/Login.vue'
+import Roles from '../views/roles/Roles.vue'
+import { isAuthenticated } from '../services/authenticationService'
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    children: [
+      {
+        path: 'roles', // esto crea /roles dentro de Home
+        component: Roles
+      }
+    ],
+    beforeEnter: async (to, from, next) => {
+      const valid = await isAuthenticated()
+      if (valid) {
+        next()
+      } else {
+        next('/login')
+      }
+    }
+  },
+
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ guardia-client/src/views/home/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <router-link to="/">Inicio</router-link> |
+    <router-link to="/roles">Roles</router-link> |
+    <router-link to="/" @click.prevent="handleLogout">Cerrar sesión</router-link>
+  </nav>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { logout } from '../../services/authenticationService'
+
+const router = useRouter()
+
+const handleLogout = async () => {
+  try {
+    await logout()
+  } catch (err) {
+    console.error('Error al cerrar sesión:', err)
+  } finally {
+    localStorage.removeItem('token')
+    localStorage.removeItem('username')
+    router.push('/login')
+  }
+}
+</script>
+
